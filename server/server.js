7c80be0efc34199cd1285693d181d0aa855c11ee
Alex Thorne <alexandra.dr.thorne@gmail.com>
2024-03-30 12:23:38 -0700

    mongoDB

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const userController = require("./controllers/userController");
 const cookieController = require("./controllers/cookieController");
 const sessionController = require("./controllers/sessionController");
 
-const uri = 'your_connection_string'; // Replace with your actual connection string
+const uri = 'mongodb+srv://AlexaThr:DECQSYvO5N6SfYP1@booknookcluster.ym2hlas.mongodb.net/';
 const client = new MongoClient(uri);
 
 async function connectToMongoDB() {
