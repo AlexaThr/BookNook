30a766064355350f30510421f13cd0a17dc86761
Alex Thorne <alexandra.dr.thorne@gmail.com>
2024-03-30 17:08:08 -0700

    authentication works

--- server/server.js
+++ server/server.js
@@ -1,33 +1,18 @@
-const path = require('path');
+const mongoose = require('mongoose');
 const express = require('express');
-const cookieParser = require("cookie-parser");
-const mongoose = require("mongoose");
-const { MongoClient } = require('mongodb');
+const cookieParser = require('cookie-parser');
+const path = require('path');
+const userController = require('./controllers/userController');
+const cookieController = require('./controllers/cookieController');
+const sessionController = require('./controllers/sessionController');
 
-const userController = require("./controllers/userController");
-const cookieController = require("./controllers/cookieController");
-const sessionController = require("./controllers/sessionController");
+// MongoDB URI
+const uri = '***REMOVED***';
 
-// const uri = '***REMOVED***/?retryWrites=true&w=majority&appName=BookNookCluster';
-const uri = 'mongodb://localhost:27017';
-// const client = new MongoClient(uri);
-
-// async function connectToMongoDB() {
-//     try {
-//         await client.connect();
-//         console.log('Connected to MongoDB');
-//     } catch (error) {
-//         console.error('Error connecting to MongoDB:', error);
-//     }
-// }
-
-// Call the connectToMongoDB function to establish the connection
-// connectToMongoDB();
-
-mongoose.connection.once('open', () => {
-    console.log('Connected to Database');
-});
-
+// Connect to MongoDB using Mongoose
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Error connecting to MongoDB:', err));
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -37,54 +22,42 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, '../client')));
 
-
+// Define routes
 app.get('/login', cookieController.setCookie, (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../client/login.html'))
-})
-
-
-
-app.get("/signup", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/signup.html"));
+  res.sendFile(path.resolve(__dirname, '../client/login.html'));
 });
 
-app.post(
-    "/signup", 
-    userController.createUser, 
-    cookieController.setSSIDCookie,
-    sessionController.startSession,
-    (req, res) => {
-        res.status(200).redirect("/home");
-    }
-);
-
-app.post(
-    "/login",
-    userController.verifyUser,
-    cookieController.setSSIDCookie,
-    sessionController.startSession,
-    (req, res) => {
-        res.status(200).redirect("/home");
-    }
-);
-
-app.get(
-    "/home", sessionController.isLoggedIn, (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../index.html'));
-    }
-)
-
-app.use("*", (req, res) => {
-    res.status(404).send("Not Found");
+app.get('/signup', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/signup.html'));
 });
 
+app.post('/signup', userController.createUser, cookieController.setSSIDCookie, sessionController.startSession, (req, res) => {
+  res.status(200).redirect('/home');
+});
+
+app.post('/login', userController.verifyUser, cookieController.setSSIDCookie, sessionController.startSession, (req, res) => {
+  res.status(200).redirect('/home');
+});
+
+app.get('/home', 
+    sessionController.isLoggedIn, 
+    (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client/loggedin.html'));
+    });
+
+// Handle 404 Not Found errors
+app.use('*', (req, res) => {
+  res.status(404).send('Not Found');
+});
+
+// Error handling middleware
 app.use((err, req, res, next) => {
-    console.log(err);
-    res.status(500).send({ error: err });
-})
-    
+  console.error(err);
+  res.status(500).send({ error: 'Internal Server Error' });
+});
+
 app.listen(PORT, () => {
-    console.log(`server listening on PORT ${PORT}`);
+  console.log(`Server listening on PORT ${PORT}`);
 });
 
 module.exports = app;
---
+++ server/serverbackup.js
@@ -0,0 +1,90 @@
+const path = require('path');
+const express = require('express');
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const { MongoClient } = require('mongodb');
+
+const userController = require("./controllers/userController");
+const cookieController = require("./controllers/cookieController");
+const sessionController = require("./controllers/sessionController");
+
+// const uri = '***REMOVED***/?retryWrites=true&w=majority&appName=BookNookCluster';
+const uri = 'mongodb://localhost:27017';
+// const client = new MongoClient(uri);
+
+// async function connectToMongoDB() {
+//     try {
+//         await client.connect();
+//         console.log('Connected to MongoDB');
+//     } catch (error) {
+//         console.error('Error connecting to MongoDB:', error);
+//     }
+// }
+
+// Call the connectToMongoDB function to establish the connection
+// connectToMongoDB();
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to Database');
+});
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../client')));
+
+
+app.get('/login', cookieController.setCookie, (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/login.html'))
+})
+
+
+
+app.get("/signup", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../client/signup.html"));
+});
+
+app.post(
+    "/signup", 
+    userController.createUser, 
+    cookieController.setSSIDCookie,
+    sessionController.startSession,
+    (req, res) => {
+        res.status(200).redirect("/home");
+    }
+);
+
+app.post(
+    "/login",
+    userController.verifyUser,
+    cookieController.setSSIDCookie,
+    sessionController.startSession,
+    (req, res) => {
+        res.status(200).redirect("/home");
+    }
+);
+
+app.get(
+    "/home", sessionController.isLoggedIn, (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../index.html'));
+    }
+)
+
+app.use("*", (req, res) => {
+    res.status(404).send("Not Found");
+});
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(500).send({ error: err });
+})
+    
+app.listen(PORT, () => {
+    console.log(`server listening on PORT ${PORT}`);
+});
+
+module.exports = app;
