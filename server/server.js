c9a5018120df19fcdbaa8e44a841551b16de82c4
Alex Thorne <alexandra.dr.thorne@gmail.com>
2024-08-23 17:19:18 -0700

    small change

--- server/server.js
+++ server/server.js
@@ -7,11 +7,8 @@ const cookieController = require('./controllers/cookieController');
 const sessionController = require('./controllers/sessionController');
 const bookController = require('./controllers/bookController');
 
-// MongoDB URI
-const uri = '***REMOVED***';
-
 // Connect to MongoDB using Mongoose
-mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('Error connecting to MongoDB:', err));
 
